684b2de0c581fac8e983011db31d13cc70d19396
zerochae <zerochae@kakao.com>
2022-05-22 20:15:53 +0900

    👷 Setting: typescript 절대 경로 설정

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,17 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+const path = require("path")
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        assets: path.resolve(__dirname, "src/assets"),
+        components: path.resolve(__dirname, "src/components"),
+        constants: path.resolve(__dirname, "src/constants"),
+        pages: path.resolve(__dirname, "src/pages"),
+      },
+    },
   })
 }
