a8f862e2af9a8fd0a3d532486a3f7df47ef1cca5
zerochae <zerochae@kakao.com>
2022-05-25 17:05:31 +0900

    ✨ Feat: react-helmet 추가

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,16 @@
 import Header from 'components/header';
 import Footer from 'components/footer';
+import Helmet from 'react-helmet';
+import metadata from 'hooks/getSiteMetadata';
 
 const Layout = ({ children }: any) => {
+  const { title, description } = metadata();
   return (
     <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
       <Header />
       <main>{children}</main>
       <Footer />
