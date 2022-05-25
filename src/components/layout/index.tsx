489bb2b55ba72c820ebebf543ff1c9f288ee5213
zerochae <zerochae@kakao.com>
2022-05-25 16:47:19 +0900

    ✨ Feat: layout set

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,14 @@
-const Layout = () => {
-  return <div></div>
-}
+import Header from 'components/header';
+import Footer from 'components/footer';
 
-export default Layout
+const Layout = ({ children }: any) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
