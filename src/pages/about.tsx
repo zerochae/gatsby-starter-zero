6c01206f7d3140d515a8ddf26f9bff7919810063
GrayKwon <gray.kwon@wirebarley.com>
2022-12-07 12:46:48 +0900

    ✨ Feat: about page 추가

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <div>about</div>
+    </Layout>
+  )
+}
+
+export default AboutPage
