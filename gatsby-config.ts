4101f48e41b511d8f8ce256ce0515908ed29cd8c
GrayKwon <gray.kwon@wirebarley.com>
2022-12-07 12:44:11 +0900

    📦 Chore: config update

---
+++ gatsby-config.ts
@@ -0,0 +1,46 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  graphqlTypegen: true,
+  siteMetadata: {
+    title: `회색 기록`,
+    description: `그레이 개발로그`,
+    author: `Gray`,
+    siteUrl: `http://localhost:8000/`,
+    menu: ["post", "about"],
+    menuLink: ["/", "/about"],
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-prismjs`],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -38,7 +38,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const result = await graphql(`
-    query {
+    query AllMarkdownRemark {
       allMarkdownRemark {
         edges {
           node {
